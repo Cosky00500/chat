364e0941337968ab59aba7be78981f3b7d9f75ae
Cosky00500 <sebygar11@gmail.com>
2026-04-07 19:59:26 +0200

    formazione socket funzionante

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+//g++ main.cpp -o main.exe -lws2_32
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,13 +12,38 @@
 int main () {
 
     int porta;
-    int inidirizzo = AF_INET;
+    //tipo di indirizzo tipo IPV4
+    int indirizzo = AF_INET;
+    //se la connessione deve essere tcp o altro (udp)
     int ProtocolloU = SOCK_STREAM;
+    //dispositivi collegabili
     int DispCollegabili = INADDR_ANY;
+
+    std::cout << "scegli la porta da aprire: \n";
+    std::cin >> porta;
 
     WSADATA wsaData;
     int wsa = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    
+    SOCKET ascolto = socket(indirizzo , ProtocolloU, 0);
 
+    sockaddr_in servizio;
+    servizio.sin_family = indirizzo;
+    servizio.sin_addr.s_addr = DispCollegabili;
+    servizio.sin_port = htons(porta);
+
+    if(bind(ascolto,(SOCKADDR*)&servizio,sizeof(servizio))) {
+
+        std::cout << "ERRORE" << WSAGetLastError << std::endl;
+
+    }else {
+
+        std::cout << "socket formato correttamente sulla porta:" << porta << std::endl;
+
+    };
+
+    closesocket(ascolto);
+    WSACleanup();
 
 
 
